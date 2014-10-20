63192babe106b4c2792f55b6e6518c36686bc3a2
Michaël Rigart <michael@netronix.be>
2014-10-20 19:06:17 +0200

    update due to backup recovery

--- lib/social_cleaner/twitr.rb
+++ lib/social_cleaner/twitr.rb
@@ -5,7 +5,7 @@ module SocialCleaner
 
   class Twitr
 
-    attr_accessor :consumer_token, :consumer_secret, :access_token, :access_secret, :delete_after_days
+    attr_accessor :consumer_token, :consumer_secret, :access_token, :access_secret, :delete_after_days, :expire_ids
 
     def initialize(consumer_token, consumer_secret, access_token, access_secret, delete_after_days)
       self.consumer_token = consumer_token
@@ -16,24 +16,12 @@ module SocialCleaner
     end
 
     def delete_messages
-      Twitter.configure do |config|
-        config.consumer_key = consumer_token
-        config.consumer_secret = consumer_secret
-        config.oauth_token = access_token
-        config.oauth_token_secret = access_secret
-      end
+      configure_client
 
       expire_before = Time.now - (delete_after_days * 60 * 60 * 24)
-      expire_ids = []
+      self.expire_ids = []
 
       puts "Expiring all Twitter updates prior to #{expire_before.to_s}."
-
-      Twitter.user_timeline(count: 200).each do |status|
-        if status[:created_at] < expire_before
-          puts "Queueing delete status ID #{status[:id]} created at #{status[:created_at]}  (#{status[:text]})."
-          expire_ids.push(status[:id])
-        end
-      end
 
       # Now we'll sort the array, this will have the affect of putting the oldest items first in
       # the list to be deleted.
@@ -47,6 +35,30 @@ module SocialCleaner
         puts "Deleting #{delete_status}..."
         Twitter.status_destroy(delete_status)
       end
+    end
+
+  private
+
+    def configure_client
+      Twitter.configure do |config|
+        config.consumer_key = consumer_token
+        config.consumer_secret = consumer_secret
+        config.oauth_token = access_token
+        config.oauth_token_secret = access_secret
+      end
+    end
+
+    def fetch_tweets(expire_before)
+      Twitter.user_timeline(count: 200).each do |status|
+        if status[:created_at] < expire_before
+          puts "Queueing delete status ID #{status[:id]} created at #{status[:created_at]}  (#{status[:text]})."
+          self.expire_ids.push(status[:id])
+        end
+      end
+    end
+
+    def delete_tweets
+
     end
   end
 end
